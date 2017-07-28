6c2f4954947b27c8be1bf88ba4c054736283e004
kevinstar <1097666131@qq.com>
2017-07-28 15:20:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/admin/gridviewitemheight/AtypeEntity.java
@@ -0,0 +1,26 @@
+package com.example.admin.gridviewitemheight;
+
+/**
+ * Created by admin on 2017/7/28.
+ */
+
+class  AtypeEntity {
+    String atype_name;
+    String atype_kusercode;
+
+    public String getAtype_name() {
+        return atype_name;
+    }
+
+    public void setAtype_name(String atype_name) {
+        this.atype_name = atype_name;
+    }
+
+    public String getAtype_kusercode() {
+        return atype_kusercode;
+    }
+
+    public void setAtype_kusercode(String atype_kusercode) {
+        this.atype_kusercode = atype_kusercode;
+    }
+}
---
+++ app/src/main/java/com/example/admin/gridviewitemheight/MainActivity.java
@@ -0,0 +1,160 @@
+package com.example.admin.gridviewitemheight;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<AtypeEntity> atypeEntities;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final GridView gridView = (GridView) findViewById(R.id.gridview);
+        atypeEntities = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            AtypeEntity atypeEntity = new AtypeEntity();
+            atypeEntity.setAtype_kusercode("33");
+            atypeEntity.setAtype_name("你是我意思最爱的人");
+            atypeEntities.add(atypeEntity);
+        }
+        AtypeEntity atypeEntitysss = new AtypeEntity();
+        atypeEntitysss.setAtype_name("njjkjjjjjjjjjjjj是我意思最爱的人jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj");
+        atypeEntitysss.setAtype_kusercode("33333");
+        atypeEntities.add(atypeEntitysss);
+        gridView.setAdapter(new GridViewAdapter(gridView,this, atypeEntities));
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AtypeEntity hello = new AtypeEntity();
+                hello.setAtype_kusercode("jjjj");
+                hello.setAtype_name("jjjj");
+                GridViewAdapter viewAdapter = ((GridViewAdapter)gridView.getAdapter());
+                atypeEntities.clear();
+                atypeEntities.add(hello);
+                viewAdapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+    class GridViewAdapter extends BaseAdapter{
+
+        private GridView gv;
+        private Context context;
+        private List<AtypeEntity> data;
+
+        public GridViewAdapter(GridView gv, Context context, List<AtypeEntity> list) {
+            this.gv = gv;
+            this.context = context;
+            this.data = list;
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            // TODO Auto-generated method stub
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            // TODO Auto-generated method stub
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // TODO Auto-generated method stub
+
+            Holder holder;
+            if (convertView == null) {
+                holder = new Holder();
+                convertView = View.inflate(context,
+                        R.layout.gridview, null);
+
+                holder.atype_serial_num = (TextView) convertView
+                        .findViewById(R.id.atype_serial_num);
+                holder.atype_name = (TextView) convertView
+                        .findViewById(R.id.atype_name);
+                holder.atype_kusercode = (TextView) convertView
+                        .findViewById(R.id.atype_kusercode);
+                convertView.setTag(holder);
+                // 绑定listener监听器，检测convertview的height
+
+                holder.update();
+            } else {
+                holder = (Holder) convertView.getTag();
+            }
+
+            holder.atype_serial_num.setText((position + 1) + "");
+            holder.atype_name.setText(data.get(position).getAtype_name());
+            holder.atype_kusercode.setText(data.get(position).getAtype_kusercode());
+
+            holder.atype_kusercode.setTag(position);
+            // 绑定当前的item，也就是convertview
+            holder.atype_name.setTag(convertView);
+
+            return convertView;
+        }
+
+        public class Holder {
+            public TextView atype_serial_num;
+            public TextView atype_name;
+            public TextView atype_kusercode;
+
+            public void update() {
+
+                // 精确计算GridView的item高度
+
+                atype_kusercode.getViewTreeObserver().addOnGlobalLayoutListener(
+                        new ViewTreeObserver.OnGlobalLayoutListener() {
+                            public void onGlobalLayout() {
+                                int position = (Integer) atype_kusercode.getTag();
+
+                                // 这里是保证同一行的item高度是相同的！！也就是同一行是齐整的 height相等
+
+                                if (position > 0 ) {
+                                    View v = (View) atype_name.getTag();
+                                    int height = v.getHeight();
+                                    View view = gv.getChildAt(position - 1);
+                                    if (view == null) return;
+                                    int lastheight = view.getHeight();
+
+                                    // 得到同一行的最后一个item和前一个item想比较，把谁的height大，就把两者中
+                                    // height小的item的高度设定为height较大的item的高度一致，也就是保证同一
+                                    // // 行高度相等即可
+
+                                    if (height > lastheight) {
+                                        view.setLayoutParams(new GridView.LayoutParams(
+                                                GridView.LayoutParams.FILL_PARENT,
+                                                height));
+                                    } else if (height < lastheight) {
+                                        v.setLayoutParams(new GridView.LayoutParams(
+                                                GridView.LayoutParams.FILL_PARENT,
+                                                lastheight));
+                                    }
+                                }
+                            }
+                        });
+            }
+        }
+    }
+}
